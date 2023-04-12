701263995c79b70fa42d5e2139ec4e8139a4c66c
inti25 <inti25@upfi.network>
2023-04-12 12:17:15 +0700

    init

---
+++ src/components/cbNetworkList.ts
@@ -0,0 +1,38 @@
+import {QIcon, QPixmap} from "@nodegui/nodegui";
+
+const { QComboBox } = require("@nodegui/nodegui");
+import logo from '../../assets/logox200.png';
+const axios = require('axios');
+
+async function getPixmap(url: any) {
+  const { data } = await axios.get(url, { responseType: 'arraybuffer' });
+  const pixmap = new QPixmap();
+  pixmap.loadFromData(data);
+  return pixmap;
+}
+const cbNetworks = new QComboBox();
+
+async function init() {
+  const img = await getPixmap('https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Sign-check-icon.png/240px-Sign-check-icon.png');
+  cbNetworks.setObjectName("cbNetworks");
+  cbNetworks.addItem(undefined, 'comboBox item 0');
+  cbNetworks.addItem(new QIcon(img), 'comboBox item 1');
+  cbNetworks.addItem(undefined, 'comboBox item 2');
+  cbNetworks.addItem(undefined, 'comboBox item 3');
+  cbNetworks.setStyleSheet(`
+  #cbNetworks {
+    font-size: 16px;
+    font-weight: bold;
+    height: 50px;
+    width: '100%';
+  }
+  `)
+  cbNetworks.addEventListener('currentTextChanged', (text: string) => {
+    console.log('currentTextChanged: ' + text);
+  });
+  cbNetworks.addEventListener('currentIndexChanged', (index: string) => {
+    console.log('currentIndexChanged: ' + index);
+  });
+}
+init();
+export default cbNetworks
