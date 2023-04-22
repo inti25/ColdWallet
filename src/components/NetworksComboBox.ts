ca926a4776379ad099f214eb1e606b3468cfc3c9
inti25 <inti25@upfi.network>
2023-04-22 21:59:54 +0700

    add pretty-quick

--- src/components/NetworksComboBox.ts
+++ src/components/NetworksComboBox.ts
@@ -15,7 +15,7 @@ export class NetworksComboBox {
   }
 
   getView() {
-    return this.comboBox
+    return this.comboBox;
   }
 
   async loadData() {
@@ -27,11 +27,11 @@ export class NetworksComboBox {
         height: 40px;
         width: '100%';
       }
-    `)
+    `);
 
     for (const nw of this.networks) {
       const img = await getPixmap(nw.icon);
-      this.comboBox.addItem(new QIcon(img), nw.name || '');
+      this.comboBox.addItem(new QIcon(img), nw.name || "");
     }
 
     this.comboBox.addEventListener(`currentIndexChanged`, (index: number) => {
